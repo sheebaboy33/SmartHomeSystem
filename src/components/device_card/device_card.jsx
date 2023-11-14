0e9501c980a462d49c3cbbe88b404344fd007d81
Sharm Fernando <sharmfernando33@gmail.com>
2023-11-14 15:43:35 +0530

    Added the buttons

--- src/components/device_card/device_card.jsx
+++ src/components/device_card/device_card.jsx
@@ -1,11 +1,25 @@
-import s from './device-card.module.css';
+import s from "./device-card.module.css";
+import PropTypes from "prop-types";
+import { PowerIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 function DeviceCard(props) {
+    const handleClick = () => {
+        alert("Button was clicked!!!" + props.name);
+    }
+
     return (
         <div className= {s.device_card}>
             <img className={s.device_card_img} src= {props.image} alt="Light " />
             <div className={s.device_card_info}>
-                <h2 className={s.device_card_text}>{props.name}</h2>
+                <div className={s.device_card_info_container}>
+                    <h3 className={s.device_card_text}>{props.name}</h3>
+                        <button type="button"
+                            onMouseEnter={handleClick}
+                            className={s.device_card_icon_off}>
+                            <PowerIcon width={36} height={36} />
+                        </button>
+                </div>
             </div>
         </div>
     )
